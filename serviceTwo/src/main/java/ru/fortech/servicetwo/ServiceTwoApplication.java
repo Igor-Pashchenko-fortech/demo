cc78f808ba8ebef4483c0ddcf3a0f1aeb784f215
igor <pashchenko.fortech@gmail.com>
2024-05-21 12:54:00 +0300

    init project

---
+++ serviceTwo/src/main/java/ru/fortech/servicetwo/ServiceTwoApplication.java
@@ -0,0 +1,13 @@
+package ru.fortech.servicetwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceTwoApplication.class, args);
+    }
+
+}
